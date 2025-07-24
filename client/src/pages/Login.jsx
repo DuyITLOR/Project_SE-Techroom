2dcd7ba9ad4cec2be8a80058443e13c39bc6d504
DuyITLOR <153221271+DuyITLOR@users.noreply.github.com>
2025-07-24 16:25:41 +0700

    Add footer header index

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react'
 import axios from 'axios'
+import Headers from '../components/Header'
 import { useNavigate } from 'react-router-dom'
 
 const Login = () => {
@@ -25,30 +26,9 @@ const Login = () => {
 
 
   return (
-      <div className='min-h-screen mx-auto  mt-1 flex items-center justify-center'>
-          <form onSubmit={handleLogin} className='space-y-4'>
-              <h1 className='text-neutral-800 text-4xl'>Login</h1>
-              <input
-                  type="text"
-                  placeholder='Enter your username'
-                  value={userName}
-                  onChange={e => setUserName(e.target.value)}
-                  className='px-32 py-2 border border-gray  rounded-md'
-              />
-              <h1 className='text-neutral-800 text-4xl'>Password</h1>
-              <input
-                  type="password"
-                  placeholder='Enter your password'
-                  value={password}
-                  onChange={e => setPassword(e.target.value)}
-                  className='px-32 py-2 border border-gray rounded-md'
-              />
-
-              <button type="submit" className='mx-auto block mt-2 px-4 py-2 text-3xl bg-amber-200 rounded-md hover:bg-blue-600'>
-                  Login
-              </button>
-          </form>
-      </div>
+    <>
+        <Headers/> 
+    </>
   )
 }
 
