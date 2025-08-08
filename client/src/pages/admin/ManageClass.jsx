a82efecca1527526a7b3338cce4cc5d1cfe5597e
sinluvyue <sinluvyue@gmail.com>
2025-08-08 23:36:19 +0700

    Add reusable AddForm component
    Introduced a new AddForm component for dynamic form rendering, ManageClass page is being used to demo add form component

--- client/src/pages/admin/ManageClass.jsx
+++ client/src/pages/admin/ManageClass.jsx
@@ -1,7 +1,33 @@
-import React from "react";
+import React, { useState } from "react";
+import AddForm from "../../components/AddForm";
 
 const ManageClass = () => {
-  return <div>ManageClass</div>;
+  const [isOpen, setIsOpen] = useState(false);
+  const fields = [
+    {
+      label: "Name",
+      name: "name",
+      type: "text",
+      placeholder: "Enter your name",
+    },
+    {
+      label: "Email",
+      name: "email",
+      type: "email",
+      placeholder: "Enter your email",
+    },
+  ];
+
+  return (
+    <>
+      {!isOpen && (
+        <button onClick={() => setIsOpen(true)}>
+          Click here to open add form
+        </button>
+      )}
+      <AddForm fields={fields} isOpen={isOpen} setIsOpen={setIsOpen} />
+    </>
+  );
 };
 
 export default ManageClass;
