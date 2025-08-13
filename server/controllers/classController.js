6ca8eab88409df611bb23fecc1391ae0961e06c8
Nguyen Tien Quoc <ntquoc23@clc.fitus.edu.vn>
2025-08-13 15:18:14 +0700

    tạo class controller

---
+++ server/controllers/classController.js
@@ -0,0 +1,43 @@
+import Class from '../models/classModel.js';
+
+// getClass: get all class information, sort it in the order of most recent first, and pass it onto the View Components. 
+// This function will call Class.getAllClasses() in Class Model for Administrators to get all listClasses
+// and call Class.getRelatedClass() in Class Model for Students/Teachers to get listClasses that they participate in. 
+export const getClass = async (req, res) => {
+    const {userid, role} = req.body;
+    if (!userid || !role) {
+        return res.status(400).send({
+        success: false,
+        message: "Username and role cannot be empty!"
+        });
+    }
+    if (role !== 'admin' && role !== 'teacher' && role !== 'student' && role !== 'superadmin') {
+        return res.status(400).send({
+        success: false,
+        message: "Invalid role!"
+        });
+    }
+    let listClasses;
+    if (role === 'admin' || role === 'superadmin') {
+        listClasses = await Class.getAllClasses();
+    } else {
+        listClasses = await Class.getRelatedClass(userid);
+    }
+
+    if (!listClasses) {
+        return res.status(200).send({
+        success: true,
+        message: "No class found."
+        });
+    }
+
+    return res.status(200).send({
+        success: true,
+        message: "Classes retrieved successfully",
+        listClasses: listClasses
+    });
+}
+
+export default {
+    getClass
+}
