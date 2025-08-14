bfa6487c3ec04db366291e650be6b084f27142b6
DuyITLOR <153221271+DuyITLOR@users.noreply.github.com>
2025-08-14 11:17:13 +0700

    Modify the studentDashboard

--- client/src/pages/student/StudentDashboard.jsx
+++ client/src/pages/student/StudentDashboard.jsx
@@ -1,9 +1,117 @@
-import React from 'react'
+import React, { useState } from "react";
+import StudentSideBar from "../../components/StudentSideBar";
+import Header from "../../components/Header";
+import Footer from "../../components/Footer";
+import TitleBanner from "../../components/TitleBanner";
+import SearchBar from "../../components/SearchBar";
+import SummaryCard from "../../components/SummaryCard";
+import ItemCard from "../../components/ClassCard";
+
+import ClassIcon from "../../assets/users.svg?react";
+import SearchIcon from "../../assets/search.svg?react";
 
 const StudentDashboard = () => {
-  return (
-    <div>StudentDashboard</div>
-  )
-}
+  const handleSearch = async (searchTerm) => {
+    if (!searchTerm) {
+      fecthAdminsAccounts();
+    }
+    try {
+      const res = await axios.get(
+        "http://localhost:4000/api/admin/account/search",
+        {
+          params: { userID: searchTerm, role: "admin" },
+        }
+      );
+      console.log("Search results: ", res.data);
+      const list = res.data.User || [];
 
-export default StudentDashboard
+      const formatted = list.map((item, index) => ({
+        id: item.UserID || "N/A",
+        name: item.FullName || "N/A",
+        dob: item.Birthday || "N/A",
+      }));
+
+      setData(formatted);
+      console.log("Formatted search results: ", formatted);
+    } catch (err) {
+      console.error("Error searching admin accounts: ", err);
+    }
+  };
+
+  const [activate, setActivate] = useState(0);
+  const [data, setData] = useState([]);
+
+  return (
+    <>
+      <div className="fixed top-0 left-0 right-0 z-50">
+        <Header />
+      </div>
+      <div className="fixed top-0 z-40 h-screen ">
+        <StudentSideBar
+          activate={activate}
+          setActivate={setActivate}
+          current={0}
+        />
+      </div>
+
+      <div className="flex flex-col min-h-screen">
+        <div
+          className={`${
+            activate ? "pl-[80px]" : "pl-[245px]"
+          } flex flex-col w-[calc(100%-225px] justify-between pt-[72px] sm:pt-24 transition-all duration-200`}>
+          <div>
+            <div className="py-3 pr-2">
+              <TitleBanner
+                title="Classes"
+                subTitle="All necessary knowledge for you"
+                Icon={ClassIcon}
+              />
+            </div>
+            <div className="flex items-stretch py-1 gap-2 pr-2">
+              <div className="flex-1">
+                <SearchBar
+                  inputText="Enter Class Name"
+                  Icon={SearchIcon}
+                  onSearch={handleSearch}
+                />
+              </div>
+              <div>
+                <SummaryCard number={data.length} name="Total Classes" />
+              </div>
+            </div>
+          </div>
+          <div className="grid grid-cols-3 gap-3 py-3 pr-2 justify-center">
+            <ItemCard
+              ClassName={"HE"}
+              TeacherName={"Yunetrea"}
+              LastModify={"1 / 2 / 2024"}
+            />
+            <ItemCard
+              ClassName={"HE"}
+              TeacherName={"Yunetrea"}
+              LastModify={"1 / 2 / 2024"}
+            />
+            <ItemCard
+              ClassName={"HE"}
+              TeacherName={"Yunetrea"}
+              LastModify={"1 / 2 / 2024"}
+            />
+            <ItemCard
+              ClassName={"HE"}
+              TeacherName={"Yunetrea"}
+              LastModify={"1 / 2 / 2024"}
+            />
+          </div>
+        </div>
+        <div
+          className={`${
+            activate ? "w-full" : "w-[calc(100%-223px)]"
+          } transition-all duration-200 ml-auto mt-auto`}>
+          <Footer />
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default StudentDashboard;
