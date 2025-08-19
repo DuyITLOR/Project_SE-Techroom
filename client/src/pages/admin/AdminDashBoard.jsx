b198f156f1716e5602949441a7385612c6cb2be2
Bao <cjbao23@clc.fitus.edu.vn>
2025-08-19 23:27:08 +0700

    Sửa search account

--- client/src/pages/admin/AdminDashBoard.jsx
+++ client/src/pages/admin/AdminDashBoard.jsx
@@ -112,7 +112,7 @@ const AdminDashBoard = () => {
       const res = await axios.get(
         "http://localhost:4000/api/admin/account/search",
         {
-          params: { userID: searchTerm, role: "admin" },
+          params: { user: searchTerm, role: "admin" },
         }
       );
       console.log("Search results: ", res.data);
