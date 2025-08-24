2b8814ae43e11f56791983bc932f7b30dd2932ba
sinluvyue <sinluvyue@gmail.com>
2025-08-24 13:42:53 +0700

    Add FeedBackForm component and new images
    Introduced the FeedBackForm React component with initial state setup. Added two new image files to the server's public directory.

---
+++ client/src/components/FeedBackForm.jsx
@@ -0,0 +1,10 @@
+import React, { useState } from "react";
+
+const FeedBackForm = ({ ClassName, StudentID, StudentName }) => {
+  const [tag, setTag] = useState(0);
+  const [content, setContent] = useState("");
+
+  return <div></div>;
+};
+
+export default FeedBackForm;
